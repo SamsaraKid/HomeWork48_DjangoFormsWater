b13b548af6753c86b8b8b03a7d5457a3ea8b9e13
koany <koroliev@yandex.ru>
2023-09-04 22:12:16 +0300

    оформление

--- water/myforms.py
+++ water/myforms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.core.validators import RegexValidator
 
+fields = ('Имя:', 'Фамилия:', 'E-mail:', 'Телефон:', 'Длительность:', 'Объём:')
+
 
 def name_val(value):
     if value[0].islower():
@@ -9,14 +11,12 @@ def name_val(value):
         raise forms.ValidationError('Может содержать только буквы')
 
 
-
-
 class WaterOrder(forms.Form):
-    name = forms.CharField(label='Имя', validators=[name_val])
-    surname = forms.CharField(label='Фамилия', validators=[name_val])
-    email = forms.CharField(label='E-mail')
-    phone = forms.CharField(label='Телефон')
+    name = forms.CharField(label=fields[0], validators=[name_val])
+    surname = forms.CharField(label=fields[1], validators=[name_val])
+    email = forms.CharField(label=fields[2])
+    phone = forms.CharField(label=fields[3])
     terms = ((1, '1 месяц'), (3, '3 месяца'), (6, '6 месяцев'), (12, '12 месяцев'))
-    term = forms.TypedChoiceField(label='Длительность доставки', choices=terms)
+    term = forms.TypedChoiceField(label=fields[4], choices=terms)
     sizes = ((5, '5 литров'), (10, '10 литров'), (15, '15 литров'))
-    size = forms.TypedChoiceField(label='Объём баллонов', choices=sizes)
+    size = forms.TypedChoiceField(label=fields[5], choices=sizes)
--- water/views.py
+++ water/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from .myforms import WaterOrder
+from .myforms import WaterOrder, fields
 
 # Create your views here.
 
 def order(req):
     if req.POST:
         form = WaterOrder(req.POST)
+        if form.is_valid():
+            data = req.POST.dict()
+            data.pop('csrfmiddlewaretoken')
+            data = list(data.values())
+            data = {'data': zip(fields, data)}
+            print(data)
+            return render(req, 'result.html', context=data)
     else:
         form = WaterOrder()
     return render(req, 'form.html', context={'form': form})
+
